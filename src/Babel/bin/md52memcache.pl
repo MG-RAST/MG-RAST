3f17b320c47248b3a75f706ae40b6b5d50b3933d
Travis Harrison <tharriso@kursk-3.mcs.anl.gov>
2012-10-25 14:08:11 -0500

    changed to use less memory when loading

--- src/Babel/bin/md52memcache.pl
+++ src/Babel/bin/md52memcache.pl
@@ -19,7 +19,7 @@ my $options = { md5      => 1,
 		        function => 1,
 		        organism => 1,
 		        source   => 1
-	        };
+	          };
 
 my $usage = "$0 [--verbose] --mem_host <server address: default '$memhost'> --mem_key <key extension: default '$memkey'> --lca <lca file> --md5 <md5 data file> --map <annotation mapping file> --option <input type: " . join("|", keys %$options) . ">\n";
 $usage   .= "lca file (unique md5s):\tmd5, domain, phylum, class, order, family, genus, species, name, level\n";
@@ -47,23 +47,22 @@ my $mem_cache = new Cache::Memcached {'servers' => [$memhost], 'debug' => 0, 'co
 unless ($mem_cache && ref($mem_cache)) { print STDERR "Unable to connect to memcache:\n$usage"; exit 1; }
 
 if (($select eq 'md5') && $lcaf && $md5f && (-s $lcaf) && (-s $md5f)) {
-  my $md5_data = {};
-  my $curr = '';
-  my $data = {};
 
-  print STDERR "Parsing lca file ... " if ($verbose);
+  print STDERR "Parsing lca file / adding to memcache ... " if ($verbose);
   open(LCAF, "<$lcaf") || die "Can't open file $lcaf: $!\n";
   while (my $line = <LCAF>) {
     chomp $line;
     $num += 1;
     my ($md5, @taxa) = split(/\t/, $line);
     my $rank = pop @taxa;
-    $md5_data->{$md5}->{lca} = \@taxa;
+    $mem_cache->set($md5.$memkey, { lca => \@taxa }, undef); # no experiation
   }
   close LCAF;
-  print STDERR "Done parsing $num md5s\n" if ($verbose);
+  print STDERR "Done parsing / adding $num md5s\n" if ($verbose);
   
   $num = 0;
+  my $curr = '';
+  my $data = {};
   print STDERR "Parsing md5 file / adding to memcache ... " if ($verbose);
   open(MD5F, "<$md5f") || die "Can't open file $md5f: $!\n";
   while (my $line = <MD5F>) {
@@ -73,17 +72,15 @@ if (($select eq 'md5') && $lcaf && $md5f && (-s $lcaf) && (-s $md5f)) {
     # initial
     if ($curr eq '') {
       $curr = $md5;
-      $data = (exists $md5_data->{$md5}) ? $md5_data->{$md5} : {};
+      $data = $mem_cache->get($curr.$memkey) || {};
     }
     # next chunk
     if ($curr ne $md5) {
-      my $key = $curr . $memkey;
-      $mem_cache->set($key, $data, undef); # no experiation
+      $mem_cache->set($curr.$memkey, $data, undef); # no experiation
       
       # reset
-      delete $md5_data->{$curr};
       $curr = $md5;
-      $data = (exists $md5_data->{$md5}) ? $md5_data->{$md5} : {};
+      $data = $mem_cache->get($curr.$memkey) || {};
       $num += 1;
     }
     # add data
@@ -91,15 +88,6 @@ if (($select eq 'md5') && $lcaf && $md5f && (-s $lcaf) && (-s $md5f)) {
   }
   close MD5F;
   print STDERR "Done parsing / adding $num md5s\n" if ($verbose);
-  
-  $num = 0;
-  print STDERR "Adding missing to memcache ... " if ($verbose);
-  foreach my $md5 (keys %$md5_data) {
-    $num += 1;
-    my $key = $md5 . $memkey;
-    $mem_cache->set($key, $md5_data->{$md5}, undef); # no experiation
-  }
-  print STDERR "Done adding $num md5s\n" if ($verbose);
 }
 elsif (($select ne 'md5') && $mapf && (-s $mapf)) {
   my $map_data = {};
