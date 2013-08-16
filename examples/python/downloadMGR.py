060be2c383900bd079c92e36b457373329857aa0
Will Trimble <trimble@anl.gov>
2013-08-15 20:34:51 -0500

    more comments for python example

--- examples/python/downloadMGR.py
+++ examples/python/downloadMGR.py
@@ -7,14 +7,14 @@ import sys, os, re
 from optparse import OptionParser
 
 def retrieveMGRbyaccession(accession):
-    a = re.search(r"(.......\..)$", accession).group(1)
+    '''Retrieve raw data from MG-RAST API using curl and dump result into file named <accession>.gz'''
+    a = re.search(r"(\d\d\d\d\d\d\d\.\d)$", accession).group(1)
     if key == "":
         sys.stderr.write("Warning: MGR webkey not defined\n")
         s1 = "curl http://api.metagenomics.anl.gov/2/sequenceset/mgm%s-050-1/             > %s.gz"  % ( a, a ) 
     else: 
         sys.stderr.write("Using MGR webkey %s\n" % key)
         s1 = "curl 'http://api.metagenomics.anl.gov/2/sequenceset/mgm%s-050-1/?&auth=%s'  > %s.gz" % ( a, key, a )
-#    print jsonobject
     sys.stderr.write("Executing %s\n" % s1) 
     os.popen(s1)
   
@@ -23,16 +23,17 @@ if __name__ == '__main__':
 example: downloadMGR.py  MGR4440613.3'''
     parser = OptionParser(usage)
     (opts, args) = parser.parse_args()
+# Assign the value of key from the OS environment
     try:
         key = os.environ["MGRKEY"]
     except KeyError:
         key = ""
-  
+# test for correct number of arguments  
     try :
         accession = args[0]
     except IndexError:
         parser.error("accession is a required parameter\n" )
-  
+# test for accession in the right format and call retrieve MGRbyaccession   
     if re.search(r"^(\d\d\d\d\d\d\d\.\d)$", accession): 
         retrieveMGRbyaccession(accession)
     elif re.search(r"^(MGR\d\d\d\d\d\d\d.\d)$", accession): 
