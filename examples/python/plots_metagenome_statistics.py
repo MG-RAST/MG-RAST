21858120a568f2f36f8477b65c3ee955c03272f6
Travis Harrison <tharriso@dieppe-2.mcs.anl.gov>
2013-08-16 15:49:37 -0500

    updated example code to use new api format correctly

--- examples/python/plots_metagenome_statistics.py
+++ examples/python/plots_metagenome_statistics.py
@@ -5,9 +5,13 @@ plots a graph using data from the web interface'''
 import urllib, json, sys
 import numpy as np
 
+# MG-RAST API url
+API_URL = "http://api.metagenomics.anl.gov/1"
+
 # retrieve the data by sending at HTTP GET request to the MG-RAST API
 ACCESSIONNUMBER = "mgm4440613.3" # this is a public job
-some_url = "http://api.metagenomics.anl.gov/2/metagenome_statistics/%s?verbosity=full" % ACCESSIONNUMBER
+
+some_url = "%s/metagenome/%s?verbosity=stats" % (API_URL, ACCESSIONNUMBER)
 sys.stderr.write("Retrieving %s\n" % some_url)
 jsonobject = urllib.urlopen(some_url).read()
 
@@ -15,9 +19,9 @@ jsonobject = urllib.urlopen(some_url).read()
 jsonstructure = json.loads(jsonobject)
 
 # get the elements of the data that we want out of the dict of dicts..
-spectrum = np.array( jsonstructure["qc"]["kmer"]["15_mer"]["data"], dtype="float")
-lengthdistribution = np.array( jsonstructure["length_histogram"]["upload"], dtype="int")
-lengthdistribution2 = np.array( jsonstructure["length_histogram"]["post_qc"], dtype="int")
+spectrum = np.array( jsonstructure["statistics"]["qc"]["kmer"]["15_mer"]["data"], dtype="float")
+lengthdistribution = np.array( jsonstructure["statistics"]["length_histogram"]["upload"], dtype="int")
+lengthdistribution2 = np.array( jsonstructure["statistics"]["length_histogram"]["post_qc"], dtype="int")
 
 # display the first ten lines of the data table
 np.savetxt(sys.stderr, spectrum[0:10], fmt="%d", delimiter="\t")
