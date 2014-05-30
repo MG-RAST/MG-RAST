019ef65d1a724d3ea6a3927581cc4f13ab4f96c7
Travis Harrison <guumby@gmail.com>
2014-05-30 11:15:18 -0500

    update profile resource with more options

--- src/MGRAST/lib/resources/profile.pm
+++ src/MGRAST/lib/resources/profile.pm
@@ -21,63 +21,82 @@ sub new {
     $self->{name} = "profile";
     $self->{rights} = \%rights;
     $self->{cutoffs} = { evalue => '5', identity => '60', length => '15' };
-    $self->{sources} = [ $self->source->{m5nr}[0], @{$self->source->{protein}}, $self->source->{m5rna}[0], @{$self->source->{rna}}, @{$self->source->{ontology}} ];
-    $self->{attributes} = { "id"                  => [ 'string', 'unique object identifier' ],
-    	                    "format"              => [ 'string', 'format specification name' ],
-    	                    "format_url"          => [ 'string', 'url to the format specification' ],
-    	                    "type"                => [ 'string', 'type of the data in the return table (taxon, function or gene)' ],
-    	                    "generated_by"        => [ 'string', 'identifier of the data generator' ],
-    	                    "date"                => [ 'date', 'time the output data was generated' ],
-    	                    "matrix_type"         => [ 'string', 'type of the data encoding matrix (dense or sparse)' ],
-    	                    "matrix_element_type" => [ 'string', 'data type of the elements in the return matrix' ],
-    	                    "shape"               => [ 'list', [ 'integer', 'list of the dimension sizes of the return matrix' ] ],
-    	                    "rows"                => [ 'list', [ 'object', [ { 'id'       => [ 'string', 'unique identifier' ],
-    						                                                   'metadata' => [ 'hash', 'key value pairs describing metadata' ] }, "rows object" ] ] ],
-    	                    "columns"             => [ 'list', [ 'object', [ { 'id'       => [ 'string', 'unique identifier' ],
-    							                                               'metadata' => [ 'hash', 'list of metadata, contains the metagenome' ] }, "columns object" ] ] ],
-    	                    "data"                => [ 'list', [ 'list', [ 'float', 'the matrix values' ] ] ]
-                          };
+    $self->{sources} = [
+        $self->source->{m5nr}[0],
+        @{$self->source->{protein}},
+        $self->source->{m5rna}[0],
+        @{$self->source->{rna}},
+        @{$self->source->{ontology}}
+    ];
+    $self->{attributes} = {
+        "id"                  => [ 'string', 'unique object identifier' ],
+        "format"              => [ 'string', 'format specification name' ],
+        "format_url"          => [ 'string', 'url to the format specification' ],
+        "type"                => [ 'string', 'type of the data in the return table (taxon, function or gene)' ],
+        "generated_by"        => [ 'string', 'identifier of the data generator' ],
+        "date"                => [ 'date', 'time the output data was generated' ],
+        "matrix_type"         => [ 'string', 'type of the data encoding matrix (dense or sparse)' ],
+        "matrix_element_type" => [ 'string', 'data type of the elements in the return matrix' ],
+        "shape"               => [ 'list', [ 'integer', 'list of the dimension sizes of the return matrix' ] ],
+        "rows"                => [ 'list', [ 'object', [ { 'id'       => [ 'string', 'unique identifier' ],
+    						                               'metadata' => [ 'hash', 'key value pairs describing metadata' ] }, "rows object" ] ]
+    						     ],
+    	"columns"             => [ 'list', [ 'object', [ { 'id'       => [ 'string', 'unique identifier' ],
+    							                           'metadata' => [ 'hash', 'list of metadata, contains the metagenome' ] }, "columns object" ] ]
+    							 ],
+    	"data"                => [ 'list', [ 'list', [ 'float', 'the matrix values' ] ] ]
+    };
     return $self;
 }
 
 # resource is called without any parameters
 # this method must return a description of the resource
 sub info {
-  my ($self) = @_;
-  my $content = { 'name' => $self->name,
-		  'url' => $self->cgi->url."/".$self->name,
-		  'description' => "A profile in biom format that contains abundance and similarity values",
-		  'type' => 'object',
-		  'documentation' => $self->cgi->url.'/api.html#'.$self->name,
-		  'requests' => [ { 'name'        => "info",
-				    'request'     => $self->cgi->url."/".$self->name,
-				    'description' => "Returns description of parameters and attributes.",
-				    'method'      => "GET" ,
-				    'type'        => "synchronous" ,  
-				    'attributes'  => "self",
-				    'parameters'  => { 'options'     => {},
-						               'required'    => {},
-						               'body'        => {} } },
-				  { 'name'        => "instance",
-				    'request'     => $self->cgi->url."/".$self->name."/{ID}",
-				    'description' => "Returns a single data object in BIOM format",
-				    'method'      => "GET" ,
-				    'type'        => "synchronous" ,  
-				    'attributes'  => $self->attributes,
-				    'parameters'  => { 'options' => {
-				                      'evalue'   => ['int', 'negative exponent value for maximum e-value cutoff: default is '.$self->{cutoffs}{evalue}],
-      				                  'identity' => ['int', 'percent value for minimum % identity cutoff: default is '.$self->{cutoffs}{identity}],
-      				                  'length'   => ['int', 'value for minimum alignment length cutoff: default is '.$self->{cutoffs}{length}],
-									  'type' => [ 'cv', [ ['organism', 'return organism data'],
-											      ['function', 'return functional data'],
-											      ['feature', 'return feature data'] ] ],
-									  'source' => [ 'cv', $self->{sources} ]
-									},
-						       'required'    => { "id" => [ "string", "unique object identifier" ] },
-						       'body'        => {} } }
-				]
-		};
-  $self->return_data($content);
+    my ($self) = @_;
+    my $content = {
+        'name'         => $self->name,
+        'url'          => $self->cgi->url."/".$self->name,
+        'description'  => "A profile in biom format that contains abundance and similarity values",
+        'type'          => 'object',
+        'documentation' => $self->cgi->url.'/api.html#'.$self->name,
+        'requests'      => [
+            { 'name'        => "info",
+			  'request'     => $self->cgi->url."/".$self->name,
+			  'description' => "Returns description of parameters and attributes.",
+              'method'      => "GET" ,
+              'type'        => "synchronous" ,  
+              'attributes'  => "self",
+              'parameters'  => {
+                  'options'  => {},
+                  'required' => {},
+                  'body'     => {} }
+			},
+            { 'name'        => "instance",
+              'description' => "Returns a single data object in BIOM format",
+              'method'      => "GET" ,
+              'type'        => "synchronous" ,  
+              'attributes'  => $self->attributes,
+              'parameters'  => {
+                  'options' => {
+                      'evalue'   => ['int', 'negative exponent value for maximum e-value cutoff: default is '.$self->{cutoffs}{evalue}],
+                      'identity' => ['int', 'percent value for minimum % identity cutoff: default is '.$self->{cutoffs}{identity}],
+                      'length'   => ['int', 'value for minimum alignment length cutoff: default is '.$self->{cutoffs}{length}],
+                      'source'   => ['cv', $self->{sources}],
+                      'type'     => ['cv', [ ['organism', 'return organism data'],
+											  ['function', 'return functional data'],
+											  ['feature', 'return feature data'] ]
+									],
+					  'hit_type' => ['cv', [ ['all', 'returns results based on all organisms that map to top hit per read-feature'],
+                                             ['single', 'returns results based on a single organism for top hit per read-feature'],
+                                             ['lca', 'returns results based on the Least Common Ancestor for all organisms (M5NR+M5RNA only) that map to hits from a read-feature']]
+                                    ]
+				  },
+                  'required' => { "id" => ["string", "unique object identifier"] },
+                  'body'     => {} }
+            }
+        ]
+	};
+    $self->return_data($content);
 }
 
 # the resource is called with an id parameter
@@ -124,6 +143,7 @@ sub prepare_data {
     my $params = {};
     my $cgi = $self->cgi;
     $params->{type}     = $cgi->param('type') ? $cgi->param('type') : 'organism';
+    $params->{hit_type} = $cgi->param('hit_type') ? $cgi->param('hit_type') : 'all';
     $params->{source}   = $cgi->param('source') ? $cgi->param('source') : (($params->{type} eq 'organism') ? 'M5NR' : (($params->{type} eq 'function') ? 'Subsystems': 'RefSeq'));
     $params->{evalue}   = defined($cgi->param('evalue')) ? $cgi->param('evalue') : $self->{cutoffs}{evalue};
     $params->{identity} = defined($cgi->param('identity')) ? $cgi->param('identity') : $self->{cutoffs}{identity};
@@ -180,20 +200,43 @@ sub prepare_data {
     # get data
     if ($params->{type} eq 'organism') {
         $ttype = 'Taxon';
-        # my ($self, $sources, $eval, $ident, $alen, $with_taxid) = @_;
-        my ($md5_abund, $result) = $mgdb->get_organisms_for_sources([$params->{source}], int($params->{evalue}), int($params->{identity}), int($params->{length}), 1);
-        # mgid, source, tax_domain, tax_phylum, tax_class, tax_order, tax_family, tax_genus, tax_species, name, abundance, sub_abundance, exp_avg, exp_stdv, ident_avg, ident_stdv, len_avg, len_stdv, md5s, taxid
+        my $result = [];
+        if ($htype eq 'all') {
+            # my ($self, $sources, $eval, $ident, $alen, $with_taxid) = @_;
+            (undef, $result) = $mgdb->get_organisms_for_sources([$params->{source}], int($params->{evalue}), int($params->{identity}), int($params->{length}), 1);
+            # mgid, source, tax_domain, tax_phylum, tax_class, tax_order, tax_family, tax_genus, tax_species, name, abundance, sub_abundance, exp_avg, exp_stdv, ident_avg, ident_stdv, len_avg, len_stdv, md5s, taxid
+        } elsif ($htype eq 'single') {
+            # my ($self, $source, $eval, $ident, $alen, $with_taxid) = @_;
+            $result = $mgdb->get_organisms_unique_for_source($params->{source}, int($params->{evalue}), int($params->{identity}), int($params->{length}), 1);
+            # mgid, tax_domain, tax_phylum, tax_class, tax_order, tax_family, tax_genus, tax_species, name, abundance, exp_avg, exp_stdv, ident_avg, ident_stdv, len_avg, len_stdv, md5s, taxid
+        } elsif ($htype eq 'lca') {
+            # my ($self, $eval, $ident, $alen) = @_;
+            $result = $mgdb->get_lca_data(int($params->{evalue}), int($params->{identity}), int($params->{length}));
+            # mgid, tax_domain, tax_phylum, tax_class, tax_order, tax_family, tax_genus, tax_species, name, abundance, exp_avg, exp_stdv, ident_avg, ident_stdv, len_avg, len_stdv
+        }
         foreach my $row (@$result) {
-            my $tax_str = [ $row->[2], $row->[3], $row->[4], $row->[5], $row->[6], $row->[7], $row->[9] ];
-            push(@$rows, { "id" => $row->[19], "metadata" => { "taxonomy" => $tax_str }  });
-            push(@$values, [ $self->toFloat($row->[10]), $self->toFloat($row->[12]), $self->toFloat($row->[14]), $self->toFloat($row->[16]) ]);
+            if ($htype eq 'all') {
+                my $rmd = {
+                    taxonomy => [$row->[2], $row->[3], $row->[4], $row->[5], $row->[6], $row->[7], $row->[8]],
+                    tax_id   => $row->[19]
+                };
+                push(@$rows, { "id" => $row->[9], "metadata" => $rmd });
+                push(@$values, [ $self->toFloat($row->[10]), $self->toFloat($row->[12]), $self->toFloat($row->[14]), $self->toFloat($row->[16]) ]);
+            } else {
+                my $rmd = { taxonomy => [$row->[1], $row->[2], $row->[3], $row->[4], $row->[5], $row->[6], $row->[7]] };
+                if ($htype eq 'single') {
+                    $rmd->{tax_id} = $row->[17];
+                }
+                push(@$rows, { "id" => $row->[8], "metadata" => $rmd });
+                push(@$values, [ $self->toFloat($row->[9]), $self->toFloat($row->[10]), $self->toFloat($row->[12]), $self->toFloat($row->[14]) ]);
+            }
         }
     }
     elsif ($params->{type} eq 'function') {
         $ttype = 'Function';
         my $function2ont = $mgdb->get_hierarchy('ontology', $params->{source});
         # my ($self, $source, $eval, $ident, $alen) = @_;
-        my ($md5_abund, $result) = $mgdb->get_ontology_for_source($params->{source}, int($params->{evalue}), int($params->{identity}), int($params->{length}));
+        my (undef, $result) = $mgdb->get_ontology_for_source($params->{source}, int($params->{evalue}), int($params->{identity}), int($params->{length}));
         # mgid, id, annotation, abundance, sub_abundance, exp_avg, exp_stdv, ident_avg, ident_stdv, len_avg, len_stdv, md5s
         foreach my $row (@$result) {
             next unless (exists $function2ont->{$row->[1]});
