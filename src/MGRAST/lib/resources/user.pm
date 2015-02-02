c5bd0d21bb87f69b9e0813db4810664c943451be
Tobias Paczian <paczian@mcs.anl.gov>
2015-02-02 09:24:24 -0600

    udpated to enable optional mg-rast auth

--- src/MGRAST/lib/resources/user.pm
+++ src/MGRAST/lib/resources/user.pm
@@ -270,6 +270,12 @@ sub instance {
 
     # preferences were passed
     if (defined $rest->[1] && $rest->[1] eq "preferences") {
+      my $userToken = $master->Preferences->get_objects({ user => $user, name => "WebServicesKey" });
+      if (scalar(@$userToken)) {
+	$userToken = $userToken->[0]->{value};
+      } else {
+	$self->return_data( {"ERROR" => "insufficient permissions for this user call"}, 401 );
+      }
       my $prefs = { 'type' => 'preference', 'app' => 'MGRAST', 'id' => 'mgu'.$self->user->_id, "pref" => $self->json->decode($self->cgi->param('prefs')) };
       
       my $pref_id = $master->Preferences->get_objects({ user => $user, name => "shock_pref_node" });
@@ -277,9 +283,9 @@ sub instance {
       my $retval = {};
       if (scalar(@$pref_id)) {
 	$nodeid = $pref_id->[0]->{value};
-	$retval = $self->update_shock_node($nodeid, $prefs);#, $self->mgrast_token);
+	$retval = $self->update_shock_node($nodeid, $prefs, $userToken, "mgrast");
       } else {
-	$retval = $self->set_shock_node("preferences", undef, $prefs);#, $self->mgrast_token);
+	$retval = $self->set_shock_node("preferences", undef, $prefs, $userToken, undef, "mgrast");
 	$master->Preferences->create({ user => $user, name => "shock_pref_node", value => $retval->{id} });
       }
       $self->return_data( {"OK" => $retval->{attributes}->{pref}}, 200 );
