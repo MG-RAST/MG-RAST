64a45860ab09a599c44b8255ee1848e6457ca507
Tobias Paczian <paczian@mcs.anl.gov>
2015-09-24 07:55:00 -0500

    corrected typo

--- src/MGRAST/lib/resources/inbox.pm
+++ src/MGRAST/lib/resources/inbox.pm
@@ -406,7 +406,7 @@ sub seq_stats {
     
     my $node = $self->node_from_inbox_id($uuid, $self->token, $self->user_auth);
     if (exists($node->{attributes}{data_type}) && ($node->{attributes}{data_type} eq "sequence")) {
-        $response->{status} .= " has already been ran";
+        $response->{status} .= " has already been run";
         $self->return_data($response);
     }
     
