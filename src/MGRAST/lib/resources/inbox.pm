e637c25ee5e03c2748eed47e852cece4d41fb215
Tobias Paczian <paczian@mcs.anl.gov>
2015-09-24 08:05:48 -0500

    fixed bugs in stdout document retrieval

--- src/MGRAST/lib/resources/inbox.pm
+++ src/MGRAST/lib/resources/inbox.pm
@@ -609,9 +609,9 @@ sub view_inbox_actions {
         $params->{state} = $requestedStates;
     }
     my $data = $self->get_awe_query($params, $self->token, $self->user_auth);
-    foreach my $doc (@$data) {
+    foreach my $doc (@{$data->{data}}) {
       if ($doc->{lastfailed}) {
-	$doc->{stdout} = $self->get_awe_report($doc->{lastfailed}, "stdout", $self->token, $self->user_auth) = @_;
+	$doc->{stdout} = $self->get_awe_report($doc->{lastfailed}, "stdout", $self->token, $self->user_auth);
       }
     }
     
