4797f9828024f18881615d87197a78068b05c612
Travis Harrison <guumby@gmail.com>
2016-05-03 11:33:20 -0500

    refactor for better memory management

--- src/MGRAST/lib/resources/profile.pm
+++ src/MGRAST/lib/resources/profile.pm
@@ -266,19 +266,32 @@ sub prepare_data {
     unless (exists $all_srcs->{ $params->{source} }) {
         return ({"ERROR" => "Invalid source for profile call of type ".$params->{type}.": ".$params->{source}." - valid types are [".join(", ", keys %$all_srcs)."]"}, 400);
     }
-
-    my $values  = [];
-    my $rows    = [];
-    my $ttype   = '';
-    my $columns = [ { id => 'abundance', metadata => {metagenome => "mgm".$id} },
-		            { id => 'e-value', metadata => {metagenome => "mgm".$id} },
-		            { id => 'percent identity', metadata => {metagenome => "mgm".$id} },
-		            { id => 'alignment length', metadata => {metagenome => "mgm".$id} }
-		          ];
+    
+    # set profile
+    my $columns = [
+        { id => 'abundance', metadata => {metagenome => "mgm".$id} },
+        { id => 'e-value', metadata => {metagenome => "mgm".$id} },
+        { id => 'percent identity', metadata => {metagenome => "mgm".$id} },
+        { id => 'alignment length', metadata => {metagenome => "mgm".$id} }
+	];
+	my $profile = {
+        "id"                  => "mgm".$id.'_'.$params->{type}.'_'.$params->{source},
+        "format"              => "Biological Observation Matrix 1.0",
+        "format_url"          => "http://biom-format.org",
+        "type"                => "",
+        "generated_by"        => "MG-RAST revision ".$Conf::server_version,
+        "date"                => strftime("%Y-%m-%dT%H:%M:%S", localtime),
+        "matrix_type"         => "dense",
+        "matrix_element_type" => "float",
+        "shape"               => [ 0, 4 ],
+        "data"                => [],
+        "rows"                => [],
+        "columns"             => $columns
+	};
 
     # get data
     if ($params->{type} eq 'organism') {
-        $ttype = 'Taxon';
+        $profile->{type} = 'Taxon table';
         my $result = [];
         if ($params->{hit_type} eq 'all') {
             # my ($self, $sources, $eval, $ident, $alen, $with_taxid) = @_;
@@ -296,17 +309,18 @@ sub prepare_data {
         foreach my $row (@$result) {
             if ($params->{hit_type} eq 'all') {
                 my $rmd = { taxonomy => [$row->[2], $row->[3], $row->[4], $row->[5], $row->[6], $row->[7], $row->[8]] };
-                push(@$rows, { "id" => $row->[9], "metadata" => $rmd });
-                push(@$values, [ $self->toFloat($row->[10]), $self->toFloat($row->[12]), $self->toFloat($row->[14]), $self->toFloat($row->[16]) ]);
+                push(@{$profile->{rows}}, { "id" => $row->[9], "metadata" => $rmd });
+                push(@{$profile->{data}}, [ $self->toFloat($row->[10]), $self->toFloat($row->[12]), $self->toFloat($row->[14]), $self->toFloat($row->[16]) ]);
             } else {
                 my $rmd = { taxonomy => [$row->[1], $row->[2], $row->[3], $row->[4], $row->[5], $row->[6], $row->[7]] };
-                push(@$rows, { "id" => $row->[8], "metadata" => $rmd });
-                push(@$values, [ $self->toFloat($row->[9]), $self->toFloat($row->[10]), $self->toFloat($row->[12]), $self->toFloat($row->[14]) ]);
+                push(@{$profile->{rows}}, { "id" => $row->[8], "metadata" => $rmd });
+                push(@{$profile->{data}}, [ $self->toFloat($row->[9]), $self->toFloat($row->[10]), $self->toFloat($row->[12]), $self->toFloat($row->[14]) ]);
             }
         }
+        
     }
     elsif ($params->{type} eq 'function') {
-        $ttype = 'Function';
+        $profile->{type} = 'Function table';
         my $function2ont = $mgdb->get_hierarchy('ontology', $params->{source});
         # my ($self, $source, $eval, $ident, $alen) = @_;
         my (undef, $result) = $mgdb->get_ontology_for_source($params->{source}, $params->{evalue}, $params->{identity}, $params->{length});
@@ -314,74 +328,57 @@ sub prepare_data {
         foreach my $row (@$result) {
             next unless (exists $function2ont->{$row->[1]});
             my $ont_str = [ map { defined($_) ? $_ : '-' } @{$function2ont->{$row->[1]}} ];
-            push(@$rows, { "id" => $row->[1], "metadata" => { "ontology" => $ont_str } });
-            push(@$values, [ $self->toFloat($row->[3]), $self->toFloat($row->[5]), $self->toFloat($row->[7]), $self->toFloat($row->[9]) ]);
+            push(@{$profile->{rows}}, { "id" => $row->[1], "metadata" => { "ontology" => $ont_str } });
+            push(@{$profile->{data}}, [ $self->toFloat($row->[3]), $self->toFloat($row->[5]), $self->toFloat($row->[7]), $self->toFloat($row->[9]) ]);
         }
     }
     elsif ($params->{type} eq 'feature') {
-        $ttype = 'Gene';
+        $profile->{type} = 'Gene table';
         my $id2ann = {}; # md5_id => { accession => [], function => [], organism => [], ontology => [] }
         my $id2md5 = {}; # md5_id => md5
-        # mgid, md5_id, abundance, exp_avg, exp_stdv, ident_avg, ident_stdv, len_avg, len_stdv
-        my $result = $mgdb->get_md5_data(undef, $params->{evalue}, $params->{identity}, $params->{length}, 1);
-        my @md5s   = map { $_->[1] } @$result;
         my %ontol  = map { $_->[0], 1 } @{$mgdb->sources_for_type('ontology')};
-        # handle grouped sources
+        
         my $qsource = $params->{source};
         if (($params->{source} eq 'ALL') || ($params->{source} eq 'M5NR') || ($params->{source} eq 'M5RNA')) {
             $qsource = undef;
         }
-        # query cassandra m5nr
+    	
+        # cass handle
         my $chdl = $self->cassandra_m5nr_handle("m5nr_v".$mgdb->_version, $Conf::cassandra_m5nr);
-        my $iter = natatime $self->{batch_size}, @md5s;
-        while (my @curr = $iter->()) {
-            my $cass_data = $chdl->get_records_by_id(\@curr, $qsource);
-            foreach my $info (@$cass_data) {
-                # filter m5nr / m5rna results
-                if (($params->{source} eq 'M5NR') && (! $info->{is_protein})) {
-                    next;
-                }
-                if (($params->{source} eq 'M5RNA') && $info->{is_protein}) {
-                    next;
-                }
-                $id2md5->{$info->{id}} = $info->{md5};
-                if (exists $ontol{$info->{source}}) {
-                    $id2ann->{$info->{id}}{$info->{source}}{ontology} = $info->{accession};
-                } else {
-                    $id2ann->{$info->{id}}{$info->{source}}{accession} = $info->{accession};
-                }
-                if ($info->{function}) {
-                    $id2ann->{$info->{id}}{$info->{source}}{function} = $info->{function};
-                }
-                if ($info->{organism}) {
-                    $id2ann->{$info->{id}}{$info->{source}}{organism} = $info->{organism};
-                }
+        
+        # build queries
+        my $eval  = (defined($params->{evalue})   && ($params->{evalue}   =~ /^\d+$/))  ? "exp_avg <= ".($params->{length} * -1) : "";
+        my $ident = (defined($params->{identity}) && ($params->{identity} =~ /^\d+$/))  ? "ident_avg >= ".$params->{length}      : "";
+        my $alen  = (defined($params->{length})   && ($params->{length}   =~ /^\d+$/))  ? "len_avg >= ".$params->{length}        : "";
+        
+        my $query = "SELECT md5, abundance, exp_avg, len_avg, ident_avg FROM ".$mgdb->_jtbl->{md5};
+        $query .= $mgdb->_get_where_str($mgdb->_qver, "job = ".$data->{job_id}, $eval, $ident, $alen);
+        my $sth = $mgdb->_dbh->prepare($query);
+        $sth->execute() or die "Couldn't execute statement: ".$sth->errstr;
+        
+        # loop through results and build profile
+        my $md5_set = {};
+        my $batch_count = 0;
+        while (my @row = $sth->fetchrow_array()) {
+            my ($md5, $abun, $eval, $ident, $alen) = @row;
+            $md5_set->{$md5} = [$abun, $eval, $ident, $alen];
+            $batch_count++;
+            if ($batch_count == $self->{batch_size}) {
+                $self->append_profile($chdl, $profile, $md5_set, $qsource, $params->{source}, \%ontol);
+                $md5_set = {};
+                $batch_count = 0;
             }
         }
-        $chdl->close();
-        # add metadata to rows
-        foreach my $row (@$result) {
-            my $mid = $row->[1];
-            next unless (exists $id2md5->{$mid});
-            push(@$rows, { "id" => $id2md5->{$mid}, "metadata" => $id2ann->{$mid} });
-            push(@$values, [ $self->toFloat($row->[2]), $self->toFloat($row->[3]), $self->toFloat($row->[5]), $self->toFloat($row->[7]) ]);
+        if ($batch_count > 0) {
+            $self->append_profile($chdl, $profile, $md5_set, $qsource, $params->{source}, \%ontol);
         }
+        # cleanup
+        $sth->finish;
+        $mgdb->_dbh->commit;
+        $chdl->close();
     }
-  
-    my $obj = {
-        "id"                  => "mgm".$id.'_'.$params->{type}.'_'.$params->{source},
-        "format"              => "Biological Observation Matrix 1.0",
-        "format_url"          => "http://biom-format.org",
-        "type"                => $ttype." table",
-        "generated_by"        => "MG-RAST revision ".$Conf::server_version,
-        "date"                => strftime("%Y-%m-%dT%H:%M:%S", localtime),
-        "matrix_type"         => "dense",
-        "matrix_element_type" => "float",
-        "shape"               => [ scalar(@$values), 4 ],
-        "rows"                => $rows,
-        "columns"             => $columns,
-        "data"                => $values
-	};
+	
+	$profile->{shape}[0] = scalar(@{$profile->{rows}});
 	
 	# cach it in shock if right type
 	if ($shock_cached) {
@@ -418,14 +415,46 @@ sub prepare_data {
 	    }
 	    # create new node
 	    else {
-	        $node = $self->set_shock_node($obj->{id}.'.biom', $obj, $attr, $self->mgrast_token);
+	        $node = $self->set_shock_node($profile->{id}.'.biom', $profile, $attr, $self->mgrast_token);
 	    }
 	    if ($data->{public}) {
 	        $self->edit_shock_public_acl($node->{id}, $self->mgrast_token, 'put', 'read');
 	    }
 	}
     
-    return ($obj, undef);
+    return ($profile, undef);
+}
+
+sub append_profile {
+    my ($self, $chdl, $profile, $md5_set, $qsource, $source, $ontol);
+    
+    my @md5s = map { $_->[0] } @$md5_set;
+    my $cass_data = $chdl->get_records_by_id(\@md5s, $qsource);
+    foreach my $info (@$cass_data) {
+        # filter m5nr / m5rna results
+        if (($source eq 'M5NR') && (! $info->{is_protein})) {
+            next;
+        }
+        if (($source eq 'M5RNA') && $info->{is_protein}) {
+            next;
+        }
+        # add data
+        push @{$profile->{data}}, $md5_set->{$info->{id}};
+        # add row
+        my $row = { "id" => $info->{md5}, "metadata" => {} };
+        if (exists $ontol->{$info->{source}}) {
+            $row->{metadata}{$info->{source}}{ontology} = $info->{accession};
+        } else {
+            $row->{metadata}{$info->{source}}{accession} = $info->{accession};
+        }
+        if ($info->{function}) {
+            $row->{metadata}{$info->{source}}{function} = $info->{function};
+        }
+        if ($info->{organism}) {
+            $row->{metadata}{$info->{source}}{organism} = $info->{organism};
+        }
+        push @{$profile->{rows}}, $row;
+    }
 }
 
 sub check_static_profile {
