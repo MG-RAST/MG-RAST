249eb59932088f47f62d52ec6dbe8a5f86f8ace6
Travis Harrison <guumby@gmail.com>
2016-08-17 11:40:45 -0500

    use smart host in perl code and not sendmail

--- src/MGRAST/lib/resources/project.pm
+++ src/MGRAST/lib/resources/project.pm
@@ -763,7 +763,7 @@ sub updateRight {
 	$ubody->param('LINK', $WebConfig::APPLICATION_URL."?page=ClaimToken&token=$token&type=project");
 	$ubody->param('APPLICATION_NAME', $WebConfig::APPLICATION_NAME);
 	
-	my $mailer = Mail::Mailer->new();
+	my $mailer = Mail::Mailer->new('smtp', Server => $Conf::smtp_host);
 	if ($mailer->open({ From    => $WebConfig::ADMIN_EMAIL,
 			    To      => $user,
 			    Subject => $WebConfig::APPLICATION_NAME.' - new data available',
