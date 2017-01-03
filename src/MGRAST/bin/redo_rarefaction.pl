dc6bf683ec3a139dd8fa98b146eabb077ecb1c53
Travis Harrison <guumby@gmail.com>
2017-01-03 15:39:33 -0600

    finish rarefaction fix script

--- src/MGRAST/bin/redo_rarefaction.pl
+++ src/MGRAST/bin/redo_rarefaction.pl
@@ -11,6 +11,7 @@ use HTTP::Request;
 
 my $mgids  = "";
 my $mgfile = "";
+my $shock  = "http://shock.metagenomics.anl.gov";
 my $apiurl = "";
 my $token  = "";
 my $usage  = qq($0
@@ -55,7 +56,84 @@ $json->max_size(0);
 $json->allow_nonref;
 
 foreach my $mgid (@mg_list) {
-
+    # get stats node id, verify in system
+    my $snid = "";
+    eval {
+        my $get = $agent->get($apiurl.'/download/'.$mgid."?stage=done", ('Authorization', "mgrast $token"));
+        my $info = $json->decode( $get->content );
+        $snode = $info->{data}[0]{node_id};
+    };
+    unless ($snid) {
+        print STDERR "ERROR: unable to get statistics node for $mgid from API\n";
+        next;
+    }
+    # get stats node and file
+    my $snode = undef;
+    my $sobj = undef;
+    eval {
+        my $get = $agent->get($shock.'/node/'.$snid, ('Authorization', "mgrast $token"));
+        my $info = $json->decode( $get->content );
+        $snode = $info->{data};
+    };
+    eval {
+        my $get = $agent->get($shock.'/node/'.$snid."?download", ('Authorization', "mgrast $token"));
+        $sobj = $json->decode( $get->content );
+    };
+    unless ($snode && $sobj) {
+        print STDERR "ERROR: unable to get statistics node for $snid ($mgid) from Shock\n";
+        next;
+    }
+    # compute rarefaction
+    my $rare = undef;
+    my $alpha = undef;
+    eval {
+        my $get = $agent->get($apiurl.'/compute/rarefaction/'.$mgid."?asynchronous=1&alpha=1&level=species&ann_ver=1", ('Authorization', "mgrast $token"));
+        my $info = $json->decode( $get->content );
+        while ($info->{status} ne 'done') {
+            sleep 30;
+            $get = $agent->get($info->{url});
+            $info = $json->decode( $get->content );
+        }
+        $rare = $info->{data}{rarefaction};
+        $alpha = $info->{data}{alphadiversity};
+    };
+    unless ($rare && $alpha) {
+        print STDERR "ERROR: unable to compute rarefaction for $mgid from API\n";
+        next;
+    }
+    $sobj->{rarefaction} = $rare;
+    # post new node with stats attributes and file
+    my $status = undef;
+    eval {
+        my $content = {
+            attributes => [undef, $snode->{file}{name}, Content => $self->json->encode($snode->{attributes})],
+            upload => [undef, "attributes", Content => $self->json->encode($sobj)]
+        };
+        my @args = (
+            'Authorization', "mgrast $token",
+            'Content_Type', 'multipart/form-data',
+            'Content', $content
+        );
+        my $post = $self->agent->post($shock.'/node', @args);
+        $status = $self->json->decode( $post->content );
+    };
+    unless ($status) {
+        print STDERR "ERROR: unable to POST new statistics node for $mgid to Shock\n";
+        next;
+    }
+    print STDOUT "New stats node ".$status->{id}." created\n";
+    # delete old stats node
+    $status = undef;
+    eval {
+        my $del = $agent->delete($shock.'/node/'.$snid, ('Authorization', "mgrast $token"));
+        $status = $self->json->decode( $del->content );
+    };
+    unless ($status) {
+        print STDERR "ERROR: unable to DELETE old statistics node $snid for $mgid from Shock\n";
+        next;
+    }
+    print STDOUT "Old stats node $snid deleted\n";
 }
 
+exit 0;
 
