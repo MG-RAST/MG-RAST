ce7c100e7def0f24543ec224cba63ce4626c0098
Travis Harrison <guumby@gmail.com>
2018-09-11 14:59:50 -0500

    no url auth allowed

--- src/MGRAST/lib/resources/search.pm
+++ src/MGRAST/lib/resources/search.pm
@@ -191,7 +191,7 @@ sub query {
         if ( $p =~ /^\_/ ) {
             next;
         }
-        unless (exists($self->{query_opts}{$p}) || exists($self->{fields}{$p}) || ($p eq 'auth')) {
+        unless (exists($self->{query_opts}{$p}) || exists($self->{fields}{$p})) {
             $self->return_data( { "ERROR" => "Invalid parameter: $p" }, 404 );
         }
     }
